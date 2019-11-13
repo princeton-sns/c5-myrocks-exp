b364b3cc7d4eb87e96362e3f2fd3319cf8628142
Jeffrey Helt <jmhelt13@gmail.com>
2019-11-13 14:32:28 -0500

    Collect seconds behind master.

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -11,23 +11,28 @@ fi
 cd $builddir
 
 while true; do
-    out=$(./bin/mysqladmin --defaults-file=$cnf extended-status)
+    [[ $cnf == *"slave.cnf"* ]] && masterslave="slave" || masterslave="master"
+
+    statusout=$($scriptsdir/tools/read_status.sh $builddir $cnf $masterslave)
+    adminout=$(./bin/mysqladmin --defaults-file=$cnf extended-status)
     t=$(date +%s%3N)
 
-    echo "$out" | grep Com_commit | awk -F' ' '{ print $4 }' \
-	| xargs printf "$t,com_commit,%s\n"
+    echo "$adminout" | grep Com_commit | awk -F' ' '{ print $4 }' \
+	      | xargs printf "$t,com_commit,%s\n"
 
-    if [[ $cnf == *"slave.cnf"* ]]; then
-  echo "$out" | grep Com_queued | awk -F' ' '{ print $4 }' \
-      | xargs printf "$t,com_queued,%s\n"
-  echo "$out" | grep Com_dequeued | awk -F' ' '{ print $4 }' \
-      | xargs printf "$t,com_dequeued,%s\n"
-	echo "$out" | grep Slave_producer_scheduled | awk -F' ' '{ print $4 }' \
-    	    | xargs printf "$t,producer_scheduled,%s\n"
-	echo "$out" | grep Slave_dependency_next_waits | awk -F' ' '{ print $4 }' \
-    	    | xargs printf "$t,next_waits,%s\n"
+    if [[ $masterslave == "slave" ]]; then
+        echo "$adminout" | grep Com_queued | awk -F' ' '{ print $4 }' \
+            | xargs printf "$t,com_queued,%s\n"
+        echo "$adminout" | grep Com_dequeued | awk -F' ' '{ print $4 }' \
+            | xargs printf "$t,com_dequeued,%s\n"
+	      echo "$adminout" | grep Slave_producer_scheduled | awk -F' ' '{ print $4 }' \
+    	      | xargs printf "$t,producer_scheduled,%s\n"
+	      echo "$adminout" | grep Slave_dependency_next_waits | awk -F' ' '{ print $4 }' \
+    	      | xargs printf "$t,next_waits,%s\n"
+
+        echo "$statusout" | tr "\n" "\t" | cut -f81 | xargs printf "$t,secs_lags,%s\n"
     fi
-    
+
     sleep 0.1
 done
 
