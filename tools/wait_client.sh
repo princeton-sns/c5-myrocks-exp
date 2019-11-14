0c39b38fa96e342172299e60597be85d64b6582f
Jeffrey Helt <jmhelt13@gmail.com>
2019-11-14 12:07:51 -0500

    Small fix.

--- tools/wait_client.sh
+++ tools/wait_client.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 while [[ $(ps aux | grep oltpbench | wc -l) -gt 1 ]]; do
     sleep 0.2
--- tools/wait_transfer.sh
+++ tools/wait_transfer.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 projectdir=$1
 builddir=$2
