c6f5dc0e4d1c12bf36c2c3c91d15b24fd4bd15e4
Jeffrey Helt <jmhelt13@gmail.com>
2020-01-30 11:48:28 -0500

    Add new ro microbenchmark plot.

---
+++ plotting/insert_ro_plot.R
@@ -0,0 +1,143 @@
+#!/usr/bin/env Rscript
+library("argparse")
+library("dplyr")
+library("forcats")
+library("ggplot2")
+library("ggsci")
+library("readr")
+library("scales")
+library("tidyr")
+
+parser <- ArgumentParser()
+
+parser$add_argument("-o", "--out", type = "character", help = "out file for plot")
+parser$add_argument("csvs",
+  metavar = "csv", type = "character", nargs = "+",
+  help = "list of csv files to read in"
+)
+
+args <- parser$parse_args()
+
+out <- args$out
+
+data <- tibble()
+for (csv in args$csvs) {
+  data <- bind_rows(data, read_csv(csv))
+}
+
+barplot <- function(data, x = x, y = y,fill = fill,
+                    ylims = NULL, ybreaks = NULL,
+                    xtitle = NULL, ytitle = NULL, filltitle = NULL) {
+  g <- ggplot(
+    data,
+    aes(
+        x = as.factor(!!enquo(x)),
+        y = !!enquo(y),
+        fill = as.factor(!!enquo(fill))
+    )
+  )
+
+  if (is.null(ybreaks)) {
+    ybreaks <- waiver()
+  } else if (is.numeric(ybreaks) && length(ybreaks) == 1) {
+    ybreaks <- pretty_breaks(n = ybreaks)
+  }
+
+  barwidth <- 0.9
+  errorwidth <- 0.5
+
+  g <- g +
+    geom_col(position = position_dodge(width = barwidth), color = "black") +
+    scale_y_continuous(
+      limits = ylims,
+      breaks = ybreaks,
+      expand = c(0, 0)
+    ) +
+    scale_fill_brewer(type = "div", palette = "Paired") +
+    labs(
+      x = xtitle,
+      y = ytitle,
+      fill = filltitle
+    ) +
+    theme_classic(
+      base_size = 28,
+      base_family = "serif"
+    ) +
+    theme(
+      axis.text = element_text(size = 28, color = "black"),
+      axis.title = element_text(size = 32, color = "black"),
+      legend.text = element_text(size = 28, color = "black"),
+      legend.title = element_text(size = 32, color = "black"),
+      legend.position = "top",
+      legend.justification = "left",
+      legend.margin = margin(0, 0, -10, 0),
+      panel.grid.major.y = element_line(color = "black", linetype = 2),
+      panel.spacing = unit(0, "mm"),
+      plot.margin = margin(5, 5, 10, 5),
+    )
+
+  return(g)
+}
+
+n_clients <- data %>%
+    filter(server == "Primary") %>%
+    group_by(n_clients, n_roclients) %>%
+    summarize(
+        mean_commit_rate = mean(commit_rate_tps),
+        med_commit_rate = median(commit_rate_tps)
+    ) %>%
+    group_by(n_roclients) %>%
+    mutate(
+        max_mean_commit_rate = max(mean_commit_rate),
+        max_med_commit_rate = max(med_commit_rate)
+    ) %>%
+    filter(med_commit_rate == max_med_commit_rate) %>%
+    select(n_roclients, n_clients)
+
+n_workers <- data %>%
+    semi_join(n_clients) %>%
+    filter(server != "Primary" & rw == "w") %>%
+    group_by(server, n_clients, n_workers, n_roclients) %>%
+    summarize(
+        med_relative_commit_rate = median(relative_commit_rate),
+        mean_relative_commit_rate = mean(relative_commit_rate),
+        sd = sd(relative_commit_rate),
+        se = sd(relative_commit_rate) / sqrt(n())
+    ) %>%
+    group_by(n_roclients) %>%
+    mutate(
+        max_mean_relative_commit_rate = max(mean_relative_commit_rate),
+        max_med_relative_commit_rate = max(med_relative_commit_rate)
+    ) %>%
+    filter(med_relative_commit_rate == max_med_relative_commit_rate) %>%
+    select(n_roclients, n_workers)
+
+summary <- data %>%
+    filter(server != "Primary") %>%
+    semi_join(n_clients) %>%
+    semi_join(n_workers) %>%
+    group_by(server, n_roclients, rw) %>%
+    summarize(
+        med_commit_rate = median(commit_rate_tps),
+    ) %>%
+    ungroup() %>%
+    mutate(
+        rw = as_factor(rw),
+        rw = fct_recode(rw, "Reads" = "r", "Writes" = "w"),
+        rw = fct_relevel(rw, c("Reads", "Writes")),
+        server = fct_relevel(server, c("FDR", "FDR+fRO", "FDR+kRO", "FDR+CO", "KuaFu", "KuaFu+kRO", "KuaFu+CO"))
+    ) %>%
+    select(n_roclients, med_commit_rate, rw)
+
+summary
+
+p <- barplot(summary, x = n_roclients, y = med_commit_rate, fill = rw,
+             ylims = c(0, 40000), ybreaks = 8,
+             xtitle = "Number of Read-only Clients", ytitle = "Commit Rate (Txns/sec)",
+             filltitle = "")
+
+# Output
+width <- 10 # inches
+height <- (9 / 16) * width
+
+ggsave(out, plot = p, height = height, width = width, units = "in")
--- wrangling/insert_ro_aggregate.sh
+++ wrangling/insert_ro_aggregate.sh
@@ -34,6 +34,8 @@ fi
 logsdir=$(realpath $logsdir)
 outfile=$(realpath $outfile)
 
+echo "impl,n_clients,n_workers,n_roclients,server,total_time_ms,n_commits,commit_rate_tps,relative_commit_rate,rw" > $outfile
+
 i=0
 for dir in $(find $logsdir -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
     impl=$(echo "$dir" | sed -e 's/\([^_]\+\)_.*/\1/g')
@@ -45,7 +47,7 @@ for dir in $(find $logsdir -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
 	      impl=${IMPL_NAMES[$impl]}
     fi
 
-    $SCRIPT_DIR/process_replag.py -i $logsdir/$dir/mysql_error.backup.log -o $logsdir/$dir -s $impl -c $nclients -r $nroclients -w $nworkers &
+    #$SCRIPT_DIR/process_replag.py -i $logsdir/$dir/mysql_error.backup.log -o $logsdir/$dir -s $impl -c $nclients -r $nroclients -w $nworkers &
     i=$((i+1))
 
     # Process 3 at a time
@@ -53,6 +55,36 @@ for dir in $(find $logsdir -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
         wait
         i=0
     fi
+
+    primary_csv=$(cat $logsdir/$dir/commit_rate.primary.csv)
+    backup_csv=$(cat $logsdir/$dir/commit_rate.backup.csv)
+
+    primary_cr=$(echo "$primary_csv" | awk -F',' '/primary/{ gsub("\r", "", $4); print $4 }')
+    backup_cr=$(echo "$backup_csv" | awk -F',' '/backup/{ gsub("\r", "", $4); print $4 }')
+
+    primary_rcr=$(echo "$primary_cr / $primary_cr" | bc -l)
+    backup_rcr=$(echo "$backup_cr / $primary_cr" | bc -l)
+
+    echo "$primary_csv" | \
+	      sed -e '/server/d' \
+	          -e 's/\r//' \
+	          -e "s/^primary/$impl,$nclients,$nworkers,$nroclients,Primary/" \
+	          -e "s/$/,${primary_rcr},w/" \
+	          >> $outfile
+
+    echo "$backup_csv" | \
+	      sed -e '/server/d' \
+	          -e 's/\r//' \
+	          -e "s/^backup/$impl,$nclients,$nworkers,$nroclients,$impl/" \
+	          -e "s/$/,${backup_rcr},w/" \
+	          >> $outfile
+
+    ro_reqs=$(cat $logsdir/$dir/ro/client.0.log | awk '/Rate limited reqs/{ print $10 }' | sed -e 's/[^0-9]//g')
+    ro_nanos=$(cat $logsdir/$dir/ro/client.0.log | awk '/Rate limited reqs/{ print $9 }' | sed -e 's/[^0-9]//g')
+    ro_ms=$(echo "$ro_nanos / 1000000" | bc -l)
+    ro_tput=$(cat $logsdir/$dir/ro/client.0.log | awk '/Rate limited reqs/{ print $12 }')
+
+    echo "$impl,$nclients,$nworkers,$nroclients,$impl,$ro_ms,$ro_reqs,$ro_tput,NA,r" >> $outfile
 done
 
 # Wait for all python processes to finish
